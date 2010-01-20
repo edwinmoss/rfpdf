3413c4c683dd8b830e71fc3b14329c2934889b34
Edwin Moss <edwin.moss@gmail.com>
2010-01-20 15:11:43 -0700

    Comments and cleanup.

--- lib/rfpdf/template_handlers/base.rb
+++ lib/rfpdf/template_handlers/base.rb
@@ -3,10 +3,6 @@ module RFPDF
     class Base < ::ActionView::TemplateHandlers::ERB
       
       def compile(template)
-        # "_rfpdf_compile_setup;" +
-        # "pdf = Prawn::Document.new(@rfpdf_options[:prawn]);" + 
-        # "#{template.source}\n" +
-        # "pdf.render;"
         src = "_rfpdf_compile_setup;" + super
       end
     end
